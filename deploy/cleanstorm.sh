883c1c632d18f7a32a5ed48fd1878e84ccf32bc8
julian <empty@ip.internal>
2017-03-16 16:56:04 +0000

    updated deploy scripts

--- deploy/cleanstorm.sh
+++ deploy/cleanstorm.sh
@@ -14,14 +14,14 @@ fi
 #nimbusnode=$(head -n 1 ${HOSTSFILE})
 #echo "Nimbus Node is ${nimbusnode}"
 
-#storm/cleanstormlocaldata.sh
-#storm/cleanzookeeperstate.sh
+storm/cleanstormlocaldata.sh
+storm/cleanzookeeperstate.sh
 rm -rf ${STORM_HOME}/logs/nimbus.log
 rm -rf ${STORM_HOME}/logs/ui.log
 
 cat  $fname  | while IFS='' read -r line || [ -n "$line" ] 
 do
     echo "Cleaning supervisor in ${line}"
-    ssh -n -f ${line} "${REMOTE_FOLDER}/scripts/storm/cleanstormlocaldata.sh" 
+    ssh -n -f ${line} "${YAHOOBENCH}/deploy/storm/cleanstormlocaldata.sh" 
 
 done
