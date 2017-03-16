92cce1417c4615d697866b36eb8703ac2f9538c5
julian <empty@ip.internal>
2017-03-16 17:18:20 +0000

    minor updates

--- deploy/storm/startnimbus.sh
+++ deploy/storm/startnimbus.sh
@@ -5,7 +5,7 @@ source ${DIR}/../env.sh
 
 cd ${ZOO_HOME}
 nohup bin/zkServer.sh start > /dev/null 2>&1 &
-sleep 20
+sleep 10
 cd ${STORM_HOME}
 nohup bin/storm nimbus > /dev/null 2>&1 &
 nohup bin/storm ui > /dev/null 2>&1 &
